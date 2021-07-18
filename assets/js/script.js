53fc7aeceabe933230dc232f686d51141725844f
ADE <ade.trismegisto@gmail.com>
2021-07-18 16:46:00 -0400

    style y js finales agregados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,45 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip();
+$(function(){
+    //Smooth scroll
+    $("a").click(function(event){
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var gato = this.hash;
+  
+        $("html, body").animate({
+          scrollTop: $(gato).offset().top
+        }, 800, function(){
+          window.location.hash = gato;
+        });
+      }
+    });
+
+    //Cambio de color al hacer Scroll
+    $(window).scroll(function(){
+        $('nav').toggleClass('scrolled', $(this).scrollTop() > 800);
+    });
+
+    //Tooltips
+    $('[data-toggle="tooltip"]').tooltip()
+
+    //Esconder y mostrar cards
+    $("#carta").click(function(){
+        $('#contenido').slideToggle(500);
+    });
+
+    $("#carta2").click(function(){
+        $('#contenido2').slideToggle(500);
+    });
+
+    $("#carta3").click(function(){
+        $('#contenido3').slideToggle(500);
+    });    
+
+    $("#carta4").click(function(){
+        $('#contenido4').slideToggle(500);
+    });
+
+    //Modales
+    $('[data-toggle="popover"]').popover()
 
 });
